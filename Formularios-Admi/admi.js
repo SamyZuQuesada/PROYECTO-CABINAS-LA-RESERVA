516ac51b16a5a3e3b8ed9d762bc80dd454c42016
Samy <samira.zuniga.quesada@est.una.ac.cr>
2025-11-08 23:12:28 -0600

    Api

--- Formularios-Admi/admi.js
+++ Formularios-Admi/admi.js
@@ -3,18 +3,32 @@ const baseUrl = "http://localhost:3000/landing";
 //Llama el landing completo con el servicio y testimonios
 function getLanding() {
   return fetch(baseUrl)
-    .then(response => response.json());
+    .then(response => response.json())
+    .catch(error => {
+      console.error('Error al obtener datos:', error);
+      throw error;
+    });
 }
 
-//Put
-function updateLanding(landingData) {
-  return fetch(baseUrl + IdleDeadline, {
+//Put 
+function updateLanding(id, landingData) {
+  return fetch(`${baseUrl}/${id}`, { 
     method: 'PUT',
     body: JSON.stringify(landingData),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
   })
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then((response) => {
+      if (!response.ok) throw new Error('Error en la actualización');
+      return response.json();
+    })
+    .then((json) => {
+      console.log('Datos actualizados:', json);
+      return json;
+    })
+    .catch(error => {
+      console.error('Error al actualizar:', error);
+      throw error;
+    });
 }
